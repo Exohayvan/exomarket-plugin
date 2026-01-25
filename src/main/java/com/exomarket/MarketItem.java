caab2f4273749ff3077fa60e7f10b715d8ce1a92
ExoHayvan <18trevor3695@gmail.com>
2026-01-25 15:14:22 -0500

    Removed unused import

--- src/main/java/com/exomarket/MarketItem.java
+++ src/main/java/com/exomarket/MarketItem.java
@@ -2,7 +2,6 @@ package com.exomarket;
 
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class MarketItem {
     private final ItemStack itemStack;
